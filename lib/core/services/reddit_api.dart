93c3373742b83872ec81ae343d827b8d26d75e80
Ömer Faruk Kılıç <57109630+omerfkilic@users.noreply.github.com>
2022-09-18 15:07:03 +0300

    feat: add reddit api

---
+++ lib/core/services/reddit_api.dart
@@ -0,0 +1,14 @@
+import 'package:http/http.dart' as http;
+
+String _url = 'https://www.reddit.com/r/flutterdev/top.json?count=20';
+
+Future<http.Response?> getAllPosts() async {
+  final uri = Uri.parse(_url);
+  var response = await http.get(uri);
+
+  print('response: ' + response.body);
+
+  if (response.statusCode == 200) {
+    return response;
+  }
+}
